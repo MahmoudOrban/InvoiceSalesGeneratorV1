ac552bca9787ed0a3cf207828f99c2a4105273e2
orban <mahmoud.abdelkareem@csi.mans.edu.eg>
2022-10-23 09:37:23 +0200

    Create File Operations

---
+++ src/main/java/Model/FileOperations.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mahmoud-fawzy
+ */
+public class FileOperations {
+    
+   public  ArrayList<InvoiceHeader> readFile(){
+       
+    return null;
+   }
+   public void writeFile(ArrayList<InvoiceHeader> invoices){
+       
+   }
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
